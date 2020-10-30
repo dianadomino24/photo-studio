bbae4a00b977254b646e3e11eea4b311c3f0567b
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-10-30 15:58:55 +0300

    1.2

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const { doc } = require("prettier");
-
-// const toTopButton = document.querySelector('.footer__button')
-// toTopButton.addEventListener('click', scrollTopFunction)
-
 function scrollTopFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+}
+const wrapMap = document.querySelector('.wrapMap')
+console.log(wrapMap)
+
+// создаём элемент <div>, который будем перемещать вместе с указателем мыши пользователя
+var mapTitle = document.createElement('div')
+mapTitle.className = 'mapTitle'
+// вписываем нужный нам текст внутрь элемента
+mapTitle.textContent = 'Для активации карты нажмите по ней'
+// добавляем элемент с подсказкой последним элементов внутрь нашего <div> с id wrapMap
+wrapMap.appendChild(mapTitle)
+// по клику на карту
+wrapMap.onclick = function () {
+    // убираем атрибут "style", в котором прописано свойство "pointer-events"
+    this.children[0].removeAttribute('style')
+    // удаляем элемент с интерактивной подсказкой
+    mapTitle.parentElement.removeChild(mapTitle)
+}
+// по движению мыши в области карты
+wrapMap.onmousemove = function (event) {
+    // показываем подсказку
+    mapTitle.style.display = 'block'
+    // двигаем подсказку по области карты вместе с мышкой пользователя
+    if (event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px'
+    if (event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px'
+}
+// при уходе указателя мыши с области карты
+wrapMap.onmouseleave = function () {
+    // прячем подсказку
+    mapTitle.style.display = 'none'
 }
